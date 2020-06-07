101d115948e67bb61f72f5f7b5ab82d1b6d40b67
emmanuel avwenake <imma4success@gmail.com>
2020-06-07 18:38:17 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password, salt,(err, hash)=>{
+//         console.log(hash);
+//     })
+// });
+
+var hashedPassword = '$2a$10$FQK86wHRYIfz99sR3IbjPOpiPgWqKiP7Xchao8IZLsSJiSFEZ3mbK';
+
+bcrypt.compare(password, hashedPassword,(err, res)=>{
+    console.log(res);
+})
 // var message = 'I am user number 3';
  
 // var hash = SHA256(message).toString();
 // console.log(`Message : ${message}`);
 // console.log(`Hash:${hash}`)
 
-var data = {
-    id: 10
-}
-token = jwt.sign(data,'123abc');
-console.log(token)
+// var data = {
+//     id: 10
+// }
+// token = jwt.sign(data,'123abc');
+// console.log(token)
 
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded',decoded)
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded',decoded)
