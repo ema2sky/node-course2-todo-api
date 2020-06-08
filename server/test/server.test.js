fc04fe956dea9c184147bfead721477cfff01e5f
emmanuel avwenake <imma4success@gmail.com>
2020-06-08 11:17:04 +0100

    Add test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -270,7 +270,7 @@ describe('POST /users/login', ()=>{
          })
          .end((err,res)=>{
              if (err){
-                 return done(err)
+                 return done(err);
              }
              Users.findById(users[1]._id).then((user)=>{
                  expect(user.tokens.length).toBe(0);
@@ -278,4 +278,22 @@ describe('POST /users/login', ()=>{
              }).catch((e)=>done(e));
          })
     });
-})
+});
+
+describe('DELETE /users/me/token',()=>{
+    it('should remove aut token on logout',(done)=>{
+        request(app)
+          .delete('/users/me/token')
+          .set('x-auth',users[0].tokens[0].token)
+          .expect(200)
+          .end((err,res)=>{
+              if (err){
+                  return done(err);
+              }
+              Users.findById(users[0]._id).then((user)=>{
+                  expect(user.tokens.length).toBe(0);
+                  done();
+              }).catch((e)=>done(e))
+          })
+    })
+});
